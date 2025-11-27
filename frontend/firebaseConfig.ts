e05c4947e49e23569ccf4b4317bc4f06597e4b3a
rizkipr05 <rizki.qq05@gmail.com>
2025-11-27 19:43:05 +0700

    feat: tambah nyimpen data ke firestore

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA6r0qEiPS7AP_ndRd93yPUOLy7UbaolDg", // TODO: Replace with your actual API Key
@@ -13,4 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
