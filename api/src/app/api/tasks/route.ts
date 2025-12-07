f249761afdcba3006390b29da4e5685d6bc4cdb8
Robani Diansyah <melinting522@gmail.com>
2025-12-07 23:12:20 +0700

    feat: bikin function GET, cek siapa yang login, dan siapin filter data

---
+++ api/src/app/api/tasks/route.ts
@@ -0,0 +1,29 @@
+import { Prisma } from "@/generated/prisma";
+import { getAuthUser, UNAUTHORIZED_RESPONSE } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+// Ambil semua tugas
+export async function GET(request: NextRequest) {
+    try {
+        const authUser = getAuthUser(request)
+        if (!authUser) {
+            return NextResponse.json(
+                { success: false, ...UNAUTHORIZED_RESPONSE }, 
+                { status: 401 }
+            )
+        }
+
+        // Ambil parameter query
+        const { searchParams } = new URL(request.url)
+        const status = searchParams.get('status')
+        const priority = searchParams.get('priority')
+        const course = searchParams.get('course')
+
+        const where : Prisma.TaskWhereInput = {
+            userId: authUser.id
+        }
+        if (status) where.status = status
+        if (priority) where.priority = priority
+        if (course) where.course = course
+    }
+}
