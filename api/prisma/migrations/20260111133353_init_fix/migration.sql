fbfdbfea9142a493a3f28c7340d4bd366b7e87f7
Robani Diansyah <melinting522@gmail.com>
2026-01-11 20:36:10 +0700

    fix: perbaikan sinkronisasi schema database dan reset migrasi

---
+++ api/prisma/migrations/20260111133353_init_fix/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[firebaseUid]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firebaseUid" TEXT,
+ALTER COLUMN "password" DROP NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firebaseUid_key" ON "User"("firebaseUid");
+
+-- CreateIndex
+CREATE INDEX "User_firebaseUid_idx" ON "User"("firebaseUid");
